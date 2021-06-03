5d3bef167b4371d1ee9d11175c28d8b0503a9834
Ender Bonnet <enbonnet@gmail.com>
2021-06-03 15:33:23 -0400

    update script

--- request_articles.go
+++ request_articles.go
@@ -34,7 +34,11 @@ type Article struct {
 var apiBaseUrl = "https://enbonnet-cms.herokuapp.com/"
 var allPost = "articles?_sort=created_at:desc"
 var baseDir = "./content"
-var articleRoute = baseDir + "/article"
+var articlePath = "/article"
+var postsPath = "/posts"
+var articleRoute = baseDir + articlePath
+var postsRoute = baseDir + postsPath
+var contentPaths = [2]string{articlePath, articleRoute}
 
 func write_article_file(file *os.File, article *Article) {
 	w := bufio.NewWriter(file)
@@ -56,12 +60,16 @@ func write_article_file(file *os.File, article *Article) {
 	w.Flush()
 }
 
-func main() {
-	errRmv := os.RemoveAll(articleRoute)
-	if errRmv != nil {
-		log.Fatal(errRmv)
+func remove_content() {
+	for _, path := range contentPaths {
+		errRmv := os.RemoveAll(path)
+		if errRmv != nil {
+			log.Fatal(errRmv)
+		}
 	}
+}
 
+func request_articles() []Article {
 	res, err := http.Get(apiBaseUrl + allPost)
 	if err != nil {
 		log.Fatal(err)
@@ -77,22 +85,33 @@ func main() {
 
 	var articles []Article
 	json.Unmarshal(resData, &articles)
+	return articles
+}
+
+func main() {
+	remove_content()
+	articles := request_articles()
 
 	for _, article := range articles {
 		fmt.Printf("🚀  %s\n", article.Title)
 		artId := strconv.Itoa(article.Id)
+		// I need generate this url for compatibility with old urls
 		artDir := articleRoute + "/" + artId
-		errMdr := os.MkdirAll(artDir, 0755)
-		if errMdr != nil {
-			log.Fatal(errMdr)
-		}
-
+		artDirs := [2]string{artDir, postsRoute}
 		var fileName = slugify.Slugify(article.Title)
-		file, errCreate := os.Create(artDir + "/" + fileName + ".md")
-		if errCreate != nil {
-			log.Fatal((errCreate))
-		}
 
-		write_article_file(file, &article)
+		for _, dir := range artDirs {
+			errMdr := os.MkdirAll(dir, 0755)
+			if errMdr != nil {
+				log.Fatal(errMdr)
+			}
+
+			articleFile, errCreateArticles := os.Create(dir + "/" + fileName + ".md")
+			if errCreateArticles != nil {
+				log.Fatal((errCreateArticles))
+			}
+
+			write_article_file(articleFile, &article)
+		}
 	}
 }
